085eda19d900e520ebd782d78f481c66b6c3809e
Viacheslav Oleinik <kasyava1984@gmail.com>
2023-12-08 02:26:16 +0600

    implemented the initial class description

--- src/moneyReceiver/BankCardAcceptor.java
+++ src/moneyReceiver/BankCardAcceptor.java
@@ -1,4 +1,24 @@
 package moneyReceiver;
 
-public class BankCardAcceptor {
+public class BankCardAcceptor implements MoneyReceiver{
+    private int amount;
+
+    public BankCardAcceptor(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public void addMoney() {
+
+    }
 }
