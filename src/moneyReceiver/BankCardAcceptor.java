c01d331f60fe0c1af35d9d505e99283e403d418e
Viacheslav Oleinik <kasyava1984@gmail.com>
2023-12-08 04:12:36 +0600

    Implemented the addMoney method

--- src/moneyReceiver/BankCardAcceptor.java
+++ src/moneyReceiver/BankCardAcceptor.java
@@ -1,9 +1,19 @@
 package moneyReceiver;
 
-public class BankCardAcceptor implements MoneyReceiver{
-    private int amount;
+import exceptions.CustomException;
+import util.ConsoleUtils;
 
-    public BankCardAcceptor(int amount) {
+public class BankCardAcceptor implements MoneyReceiver {
+    private int amount;
+    private int cardBalance;
+
+
+    public BankCardAcceptor() {
+        this(1200, 20);
+    }
+
+    public BankCardAcceptor(int cardBalance, int amount) {
+        this.cardBalance = cardBalance;
         this.amount = amount;
     }
 
@@ -20,5 +30,52 @@ public class BankCardAcceptor implements MoneyReceiver{
     @Override
     public void addMoney() {
 
+        while (true) {
+            String cardPAN = getCardPAN();
+            String oneTimePassword = getOneTimePassword();
+            ConsoleUtils.print("Баланс карты: " + cardBalance);
+            int sum = ConsoleUtils.getInteger("Введите сумму пополнения: ");
+            if (sum <= cardBalance) {
+                amount += sum;
+                cardBalance -= sum;
+                break;
+            } else {
+                ConsoleUtils.printError("Недостаточно средств на балансе карты.");
+            }
+        }
+
+    }
+
+    private String getOneTimePassword() {
+        String password = ConsoleUtils.getString("Введите одноразовый пароль: ");
+        try {
+            if (password.length() != 5) {
+                throw new CustomException("Вы ввели не верный пароль. Повторите ввод");
+            }
+        } catch (CustomException e) {
+            ConsoleUtils.printError(e.getMessage());
+            return getOneTimePassword();
+        }
+        return password;
+    }
+
+    private String getCardPAN() {
+        String PAN = ConsoleUtils.getString("Введите номер карты (16 цифр): ");
+        try {
+
+            if (PAN.length() != 16) {
+                throw new CustomException("Введен не верный номер карты. Повторите ввод");
+            }
+            long iPAN = Long.parseLong(PAN);
+
+
+        } catch (CustomException ce) {
+            ConsoleUtils.printError(ce.getMessage());
+            return getCardPAN();
+        } catch (NumberFormatException ne) {
+            ConsoleUtils.printError("Вы ввели не верный номер карты. Повторите ввод");
+            return getCardPAN();
+        }
+        return PAN;
     }
 }
