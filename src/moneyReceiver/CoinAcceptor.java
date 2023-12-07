b0593b0d2d5cb0656b1888eba5ac7e45ee1cf1f3
Viacheslav Oleinik <kasyava1984@gmail.com>
2023-12-08 01:57:06 +0600

    described the CoinAcceptor class

--- src/moneyReceiver/CoinAcceptor.java
+++ src/moneyReceiver/CoinAcceptor.java
@@ -1,4 +1,25 @@
 package moneyReceiver;
 
-public class CoinAcceptor {
+public class CoinAcceptor implements MoneyReceiver{
+    private int amount;
+
+    public CoinAcceptor(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public void addMoney() {
+        amount+=10;
+        System.out.println("Вы пополнили баланс на 10");
+    }
 }
